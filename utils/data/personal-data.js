3a8e7c9f3796efe5569e141c8b5724adc36b6378
saiutkarsh33 <122259179+saiutkarsh33@users.noreply.github.com>
2024-11-27 18:25:47 +0800

    Minor changes

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,19 @@
 export const personalData = {
-  name: "ABU SAID",
-  profile: '/profile.png',
-  designation: "Software Developer",
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'abusaid7388@gmail.com',
-  phone: '+8801608797655',
-  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
+  name: "Sai Utkarsh",
+  profile: "/profile.jpeg",
+  designation: "Software Engineer",
+  description:
+    "I'm passionate about leveraging technology for impactful change—to make the world around me more efficient, healthier, and safer. I've interned at Crypto.com, Mastercard, and GovTech Singapore, gaining valuable experience. I've also developed projects like Sentry Scanner, a Chrome extension that combats scams on Web2 and Web3, which won the Crypto.com 2023 APAC Hackathon, and a mobile app for KK Hospital, aiding over 500 children in obesity management. When I'm not building, you can catch me playing futsal, hosting a gathering, or tackling the new Leetcode daily challenge.",
+  email: "saiutkarsh33@gmail.com",
+  phone: "+65 86124474",
+  address: "Singapore",
+  github: "https://github.com/saiutkarsh33",
+  linkedIn: "https://www.linkedin.com/in/sai-utkarsh/",
+  leetcode: "https://leetcode.com/saiutkarsh33/",
+  twitter: "https://twitter.com/said7388",
+  stackOverflow: "https://stackoverflow.com/users/16840768/abu-said",
+  facebook: "https://www.facebook.com/abusaid.riyaz/",
   devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
-}
+  resume:
+    "https://drive.google.com/file/d/1aRgWqNf6O3DKKAujnPrQySrWJMqqgm9W/view?usp=sharing",
+};
